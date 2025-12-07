ff1918990224bd1c1ec4b69e4d654ec0ac309f06
Dmitriy <demidovgenadii@gmail.com>
2025-12-07 19:51:31 +0300

    Демидов. Изменение ПУТИ ДО бд

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./phishing.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database/phishing.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
