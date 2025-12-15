371e6d13f78bf6a899fe950b8ec7255c229a36f5
Dmitriy <demidovgenadii@gmail.com>
2025-12-15 03:58:36 +0300

    1

--- testirovanie_url.py
+++ testirovanie_url.py
@@ -2,5 +2,5 @@ from url_analyzer.analyzer import UrlAnalyzer
 import json
 
 an = UrlAnalyzer()
-res = an.analyze("https://youtube.com")
+res = an.analyze("https://google.com")
 print(json.dumps(res, indent=2, ensure_ascii=False))
